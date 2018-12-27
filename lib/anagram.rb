6ccd684cfe1d7313531a17fd521041f1013d66af
Nora Vogt <noraevogt@gmail.com>
2018-12-27 15:13:40 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    matches = []
+    split_word = @word.split("").sort
+
+    array.each do |word|
+      matches << word if word.split("").sort == split_word
+    end
+    matches
+  end
+
+# to determine anagram
+  # 1. take `word` passed in to initialize, split it, sort it, store in variable
+  # 2. iterate through match array
+  #   2.a on each iteration, split word into string, sort it, use comparison operator to compare to word above
+  # 3. if words comparison is true, add that word to the matches array
+  # 4. at the end, return the matches array
+end
